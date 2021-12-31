96ffe2da5edf6bc1619234d613e3da301c505f9b
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2021-12-31 16:36:07 +0300

    Create customErrorHandler.js

---
+++ middlewares/error/customErrorHandler.js
@@ -0,0 +1,21 @@
+const CustomError = require('../../helpers/error/CustomError');
+const customErrorHandler = async (error, req, reply, done) => {
+    let customError = error;
+    if (error.name === 'SyntaxError') {
+        customError = new CustomError('Unexpected syntax error!', 400);
+    }
+    if (error.name === 'ValidationError') {
+        customError = new CustomError(error.message, 400);
+    }
+    if (error.code === 11000) {
+        customError = new CustomError('Duplicate Key Found!: Check your email address', 400);
+    }
+    if (error.name === 'CastError') {
+        customError = new CustomError('Argument passed in must be a Buffer or string of 12 bytes or a string of 24 hex characters', 400);
+    }
+    reply.code(customError.status || 500).send({
+        success: false,
+        message: customError.message
+    });
+};
+module.exports = customErrorHandler;
