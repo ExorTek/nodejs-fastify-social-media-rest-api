88b94f7e6c49edd41b8fa6db37c300fbc1d8a807
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2021-12-31 16:35:47 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const fastify = require('fastify')({logger: true});
+const dotenv = require('dotenv');
+const connectDatabase = require('./helpers/database/connectDatabase');
+const customErrorHandler=require('./middlewares/error/customErrorHandler');
+dotenv.config({
+    path: './config/env/config.env'
+});
+connectDatabase();
+fastify.setErrorHandler(customErrorHandler);
+const PORT = process.env.PORT;
+fastify.register(require('./routers/index'), {prefix: '/api'});
+const start = async () => {
+    try {
+        await fastify.listen(PORT);
+    } catch (error) {
+        fastify.log.error(error);
+        process.exit(1);
+    }
+};
+start();
