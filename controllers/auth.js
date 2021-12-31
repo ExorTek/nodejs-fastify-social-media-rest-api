8e194b7f9a45813d5bcb032f0ddcc3a06b948e42
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2021-12-31 16:36:22 +0300

    Create auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,24 @@
+const {validateUserInputLogin, validateUserInputRegister} = require('../helpers/input/inputHelper');
+const User = require('../models/User');
+const CustomError = require('../helpers/error/CustomError');
+
+const register = async (req, reply) => {
+    try {
+        const {username, name, email, password} = req.body;
+        if (!validateUserInputRegister(username, name, email, password)) {
+            return new CustomError('Please don\'t empty the input!', 400);
+        }
+        const user = await User.create({
+            username,
+            name,
+            email,
+            password
+        });
+        reply.send({success: true, data: user})
+    }catch (err){
+        return err;
+    }
+};
+module.exports = {
+    register
+}
