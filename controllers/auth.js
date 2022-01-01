9d79065b2bff70582b5a33790dc2721017cd5572
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2022-01-01 16:31:31 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
-const {validateUserInputLogin, validateUserInputRegister} = require('../helpers/input/inputHelper');
+const {validateUserInputLogin, validateUserInputRegister, comparePassword} = require('../helpers/input/inputHelper');
 const User = require('../models/User');
 const CustomError = require('../helpers/error/CustomError');
+const {sendJwtToClient} = require("../helpers/auth/sendJwtToClient");
 
 const register = async (req, reply) => {
     const {username, name, email, password} = req.body;
@@ -13,8 +14,23 @@ const register = async (req, reply) => {
         email,
         password
     });
-    reply.send({success: true, data: user})
+    sendJwtToClient(user, reply);
 };
+const login = async (req, reply) => {
+    const {username, password} = req.body;
+    if (!validateUserInputLogin) {
+        return new CustomError('Please don\'t empty the input!', 400);
+    }
+    const user = await User.findOne({$or: [{username: username}, {email: username}]}).select('+password');
+    if (user === null) {
+        return new CustomError('Username/Email or Password is incorrect!')
+    }
+    if (!comparePassword(password, user.password)) {
+        return new CustomError('Please check the password!');
+    }
+    sendJwtToClient(user, reply);
+}
 module.exports = {
-    register
+    register,
+    login
 }
