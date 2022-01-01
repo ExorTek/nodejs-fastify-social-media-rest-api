4aca65687f3a67ffd93bf60186eb023dea0c5876
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2022-01-01 16:31:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+'use strict';
 const fastify = require('fastify')({logger: true});
 const dotenv = require('dotenv');
 const connectDatabase = require('./helpers/database/connectDatabase');
-const customErrorHandler=require('./middlewares/error/customErrorHandler');
+const customErrorHandler = require('./middlewares/error/customErrorHandler');
 dotenv.config({
     path: './config/env/config.env'
 });
