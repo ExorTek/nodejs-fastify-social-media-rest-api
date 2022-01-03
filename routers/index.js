081a38b4074713a3965a25a505893f5825da1a51
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2022-01-03 13:17:47 +0300

    delete home route

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,4 @@
 const apiRoute = async (fastify) => {
-    fastify.get('/', (req, reply) => {
-        reply.send({success: true, message: 'Welcome To Social Media RestAPI'});
-    })
     fastify.register(require('./auth/auth'), {prefix: 'auth'});
 };
 module.exports = apiRoute;
