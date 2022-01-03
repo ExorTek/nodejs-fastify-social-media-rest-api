c7cf02a272176bbdcccd3cc4755ec06601fddc8c
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2022-01-03 12:51:27 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 const {validateUserInputLogin, validateUserInputRegister, comparePassword} = require('../helpers/input/inputHelper');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const CustomError = require('../helpers/error/CustomError');
-const {sendJwtToClient} = require("../helpers/auth/sendJwtToClient");
+const {sendJwtToClient} = require("../helpers/authorization/tokenHelpers");
+const {getAccessTokenFromHeaders, isTokenIncluded} = require("../helpers/authorization/tokenHelpers");
 
 const register = async (req, reply) => {
     const {username, name, email, password} = req.body;
@@ -29,8 +31,45 @@ const login = async (req, reply) => {
         return new CustomError('Please check the password!');
     }
     sendJwtToClient(user, reply);
+};
+const getUser = async (req, reply) => {
+    const {JWT_SECRET_KEY} = process.env;
+    const accessToken = getAccessTokenFromHeaders(req);
+    if (!isTokenIncluded(req)) {
+        return new CustomError('You are not authorized to access this route', 401)
+    }
+
+    await jwt.verify(accessToken, JWT_SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return new CustomError('You are mot authorized to access this route', 401);
+        }
+        req.user = {
+            id: decoded.id,
+            createdAt: decoded.createdAt
+        };
+
+    });
+    const {id} = req.user.id;
+    const user = await User.findOne({id});
+    try {
+        return reply.code(200).send({
+            success: true,
+            data: {
+                name: user.name,
+                username: user.username,
+                title: user.title,
+                about: user.about,
+                website: user.website,
+                place: user.place,
+                profile_image: user.profile_image
+            }
+        })
+    } catch {
+        return new CustomError('You dont have Token!', 401);
+    }
 }
 module.exports = {
     register,
-    login
+    login,
+    getUser
 }
